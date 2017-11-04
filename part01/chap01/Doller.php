c5f5df506afdfbcd53a859b7fe40d05b2b2623e6
say3no <say3no@gmail.com>
2017-11-04 14:06:10 +0900

    第四章　意図を語るテスト

--- part01/chap01/Doller.php
+++ part01/chap01/Doller.php
@@ -10,7 +10,7 @@
 
 class Doller
 {
-    public $amount;
+    private $amount;
 
     function __construct(int $amount)
     {
--- part01/chap01/MoneyTest.php
+++ part01/chap01/MoneyTest.php
@@ -9,11 +9,13 @@ class MoneyTest extends \PHPUnit\Framework\TestCase
      * TODO: 1. 通貨の異なる２つの金額を足し、通貨感の為替レートにもとづいて換算された金額を得る
      *       2. 金額(通貨単位あたりの額)に数値（通貨単位数)を掛け、金額を得る
      *           ➔ １ドル100円の時５ドルを円で買う場合、100*5=500となる
-     * TODO: 2.1. amountをprivateにする
+     * 2.1. amountをprivateにする
      * 2.2.  Dollarの副作用どうする？（副作用ってなに）
      * TODO: 2.3. Moneyの丸め処理はどうする？
      * 2.4. equals()
      * TODO: 2.5. hashCode()
+     * TODO: nullとの透過性比較
+     * TODO: たのオブジェクトのとの等価性比較
      */
 
     // 1.（足し算）と2.（掛け算）では、掛け算のほうが簡単っぽいので2.から始める
@@ -21,12 +23,9 @@ class MoneyTest extends \PHPUnit\Framework\TestCase
     public function testMultiplication()
     {
         $five = new Doller(5);
-        $product = $five->times(2);
-        $this->assertEquals(10, $product->amount);
-        $product = $five->times(3);
-        $this->assertEquals(15, $product->amount);
+        $this->assertEquals((new Doller(10)), $five->times(2));
+        $this->assertEquals((new Doller(15)), $five->times(3));
     }
-
 
     // ５ドルオブジェクトはいつでも等価であることを示すテスト
     public function testEquality()
